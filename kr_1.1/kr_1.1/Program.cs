33511958e8a709de9328e611bc15fd4db0a83fbe
whopid <raguzinadarya@yandex.ru>
2024-10-04 12:06:23 +0300

    первое задани из кр, 1 вариант

---
+++ kr_1.1/kr_1.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class PalindromeChecker
+{
+    public static bool IsPalindrome(string text)
+    {
+        string cleanText = text.Replace(" ", "").ToLower();
+
+        return cleanText == new string(cleanText.Reverse().ToArray());
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строку: ");
+        string text = Console.ReadLine();
+
+
+        if (IsPalindrome(text))
+        {
+            Console.WriteLine($"'{text}' - палиндром");
+        }
+        else
+        {
+            Console.WriteLine($"'{text}' - не палиндром");
+        }
+    }
+}
