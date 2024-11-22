bf90a32a82388874a4c083e23f489388fe45d29c
whopid <raguzinadarya@yandex.ru>
2024-11-22 12:19:08 +0300

    dz 1-1

---
+++ dz1-1/dz1-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class SimpleNumbers
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите целое число:");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine("Простые числа, не превосходящие " + number + ":");
+        for (int i = 2; i <= number; i++)
+        {
+            bool simple = true;
+            for (int j = 2; j <= Math.Sqrt(i); j++)
+            {
+                if (i % j == 0)
+                {
+                    simple = false;
+                    break;
+                }
+            }
+            if (simple)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
