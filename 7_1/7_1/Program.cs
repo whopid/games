68e2280cfdb0d347cb8c6a74a82bf8d6d43005c2
whopid <raguzinadarya@yandex.ru>
2024-12-16 16:52:37 +0300

    добавила 7-1

---
+++ 7_1/7_1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class NumberValidator
+{
+    public void ValidateInput(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            throw new ArgumentException("Ничего не введено.");
+        }
+
+        if (!int.TryParse(input, out int result))
+        {
+            throw new FormatException("Неккоректное значение.");
+        }
+
+        if (result < int.MinValue)
+        {
+            throw new ArgumentOutOfRangeException("Введено слишком маленькое число.");
+        }
+
+        if (result > int.MaxValue)
+        {
+            throw new ArgumentOutOfRangeException("Введено слишком больше число.");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var validator = new NumberValidator();
+
+        Console.WriteLine("Введите число:");
+
+        string input = Console.ReadLine();
+
+        try
+        {
+            validator.ValidateInput(input);
+            Console.WriteLine("Ввод корректен.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
---
+++ 7_1/7_1/test.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class NumberValidatorTests
+{
+    private NumberValidator _validator;
+
+    [SetUp]
+    public void Setup()
+    {
+        _validator = new NumberValidator();
+    }
+
+    [Test]
+    public void ValidateInput_ValidNumber_DoesNotThrow()
+    {
+        Assert.DoesNotThrow(() => _validator.ValidateInput("100"));
+    }
+
+    [Test]
+    public void ValidateInput_TooLargeNumber_ThrowsArgumentOutOfRangeException()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => _validator.ValidateInput("2147483648"));
+    }
+
+    [Test]
+    public void ValidateInput_TooSmallNumber_ThrowsArgumentOutOfRangeException()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => _validator.ValidateInput("-2147483649"));
+    }
+
+    [Test]
+    public void ValidateInput_NonNumericInput_ThrowsFormatException()
+    {
+        Assert.Throws<FormatException>(() => _validator.ValidateInput("abc"));
+    }
+
+    [Test]
+    public void ValidateInput_EmptyInput_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() => _validator.ValidateInput(""));
+    }
+
+    [Test]
+    public void ValidateInput_WhitespaceInput_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() => _validator.ValidateInput("   "));
+    }
+}
